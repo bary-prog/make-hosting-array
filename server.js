3fa6c6be6400438dd5ea2821fc5a1e38055d792d
8KCool <8klancer@gmail.com>
2025-04-25 10:15:09 +0300

    d

--- server.js
+++ server.js
@@ -8,15 +8,21 @@ app.use(express.json()); // ✅ Parses JSON bodies
 
 app.get('/keys-values', (req, res) => {
 
-    const array_data = req.body.array_data;
-    if (!Array.isArray(array_data) || array_data.length === 0) {
-        return res.status(400).json({ error: 'Invalid or empty array_data' });
-      }
-  const obj = array_data[0];
-  const keys = Object.keys(obj);
-  const values = Object.values(obj);
+    try {
 
-  res.json({ keys, values });
+        const array_data = req.body.array_data;
+        if (!Array.isArray(array_data) || array_data.length === 0) {
+            return res.status(400).json({ error: 'Invalid or empty array_data' });
+        }
+        const obj = array_data[0];
+        const keys = Object.keys(obj);
+        const values = Object.values(obj);
+        
+        res.json({ keys, values });
+    }
+    catch(e) {
+        console.log(e.error);
+    }
 });
 
 app.listen(PORT, () => {
